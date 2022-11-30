54f2c6d049da5eb109b086aca2f3155a9f182ffa
rileyhickman <riley.hickman13@gmail.com>
2022-11-30 13:22:29 -0500

    working on moo tests

--- tests/test_multi_obj.py
+++ tests/test_multi_obj.py
@@ -3,10 +3,13 @@
 import numpy as np
 import pytest
 from olympus.campaigns import Campaign, ParameterSpace
+from olympus.datasets import Dataset
+from olympus.emulators import Emulator
 from olympus.objects import (
     ParameterCategorical,
     ParameterContinuous,
     ParameterDiscrete,
+    ParameterVector,
 )
 from olympus.scalarizers import Scalarizer
 from olympus.surfaces import Surface
@@ -63,52 +66,319 @@ MIXED_CAT_DISC_CONT = {
 SCALARIZER_KINDS = ["WeightedSum", "Parego", "Hypervolume", "Chimera"]
 
 
-# def generate_scalarizer_object(scalarizer_kind, value_space):
-#
-# 	if scalarizer_kind=='WeightedSum':
-# 		moo_params={
-# 			'weights': np.random.randint(1, 5, size=len(value_space))
-# 		}
-# 		scalarizer = Scalarizer(
-# 			kind=scalarizer_kind,
-# 			value_space=value_space,
-# 			moo_params=
-# 		)
-# 	elif scalarizer_kind=='Parego':
-# 		moo_params={}
-# 	elif scalarizer_kind=='Hypervolume':
-# 		moo_params={}
-# 	elif scalarizer_kind=='Chimera':
-# 		moo_params={
-# 			'absolutes': [False for _ in range(len(value_space))],
-# 			'tolerances': np.random.rand(len(value_space))
-# 		}
-# 	goals = np.random.choice(['min', 'max'], size=len(value_space))
-#
-#
-# 	return None
+@pytest.mark.parametrize("init_design_strategy", CONT["init_design_strategy"])
+@pytest.mark.parametrize("batch_size", CONT["batch_size"])
+@pytest.mark.parametrize("use_descriptors", CONT["use_descriptors"])
+@pytest.mark.parametrize("scalarizer_kind", SCALARIZER_KINDS)
+def test_cont(
+    init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+):
+    run_continuous(
+        init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+    )
 
 
-# def test_continuous_hypervolume():
+@pytest.mark.parametrize("init_design_strategy", DISC["init_design_strategy"])
+@pytest.mark.parametrize("batch_size", DISC["batch_size"])
+@pytest.mark.parametrize("use_descriptors", DISC["use_descriptors"])
+@pytest.mark.parametrize("scalarizer_kind", SCALARIZER_KINDS)
+def test_disc(
+    init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+):
+    run_discrete(
+        init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+    )
+
+
+@pytest.mark.parametrize("init_design_strategy", CAT["init_design_strategy"])
+@pytest.mark.parametrize("batch_size", CAT["batch_size"])
+@pytest.mark.parametrize("use_descriptors", CAT["use_descriptors"])
+@pytest.mark.parametrize("scalarizer_kind", SCALARIZER_KINDS)
+def test_cat(
+    init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+):
+    run_categorical(
+        init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+    )
+
+
+@pytest.mark.parametrize(
+    "init_design_strategy", MIXED_CAT_CONT["init_design_strategy"]
+)
+@pytest.mark.parametrize("batch_size", MIXED_CAT_CONT["batch_size"])
+@pytest.mark.parametrize("use_descriptors", MIXED_CAT_CONT["use_descriptors"])
+@pytest.mark.parametrize("scalarizer_kind", SCALARIZER_KINDS)
+def test_mixed_cat_cont(
+    init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+):
+    run_mixed_cat_cont(
+        init_design_strategy, batch_size, use_descriptors, scalarizer_kind
+    )
+
+
+def generate_scalarizer_object(scalarizer_kind, value_space):
+
+    if scalarizer_kind == "WeightedSum":
+        moo_params = {
+            "weights": np.random.randint(1, 5, size=len(value_space))
+        }
+    elif scalarizer_kind == "Parego":
+        moo_params = {}
+    elif scalarizer_kind == "Hypervolume":
+        moo_params = {}
+    elif scalarizer_kind == "Chimera":
+        moo_params = {
+            "absolutes": [False for _ in range(len(value_space))],
+            "tolerances": np.random.rand(len(value_space)),
+        }
+
+    goals = np.random.choice(["min", "max"], size=len(value_space))
+
+    scalarizer = Scalarizer(
+        kind=scalarizer_kind,
+        value_space=value_space,
+        goals=goals,
+        **moo_params,
+    )
+
+    return scalarizer, moo_params, goals
+
+
+def run_continuous(
+    init_design_strategy,
+    batch_size,
+    use_descriptors,
+    scalarizer_kind,
+    num_init_design=5,
+):
+
+    moo_surface = Surface(kind="MultFonseca")
+
+    scalarizer, moo_params, goals = generate_scalarizer_object(
+        scalarizer_kind, moo_surface.value_space
+    )
+
+    planner = BoTorchPlanner(
+        goal="minimize",
+        feas_strategy="naive-0",
+        init_design_strategy=init_design_strategy,
+        batch_size=batch_size,
+        use_descriptors=use_descriptors,
+        is_moo=True,
+        value_space=moo_surface.value_space,
+        scalarizer_kind=scalarizer_kind,
+        moo_params=moo_params,
+        goals=goals,
+    )
+
+    planner.set_param_space(moo_surface.param_space)
+
+    campaign = Campaign()
+    campaign.set_param_space(moo_surface.param_space)
+    campaign.set_value_space(moo_surface.value_space)
+
+    BUDGET = num_init_design + batch_size * 4
+
+    while len(campaign.observations.get_values()) < BUDGET:
+
+        samples = planner.recommend(campaign.observations)
+
+        for sample in samples:
+            sample_arr = sample.to_array()
+            measurement = moo_surface.run(sample_arr, return_paramvector=True)
+            campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
+
+    assert len(campaign.observations.get_params()) == BUDGET
+    assert len(campaign.observations.get_values()) == BUDGET
+    assert campaign.observations.get_values().shape[1] == len(
+        moo_surface.value_space
+    )
+
+
+def run_categorical(
+    init_design_strategy,
+    batch_size,
+    use_descriptors,
+    scalarizer_kind,
+    num_init_design=5,
+):
+
+    moo_surface = Dataset(kind="redoxmers")
+
+    scalarizer, moo_params, goals = generate_scalarizer_object(
+        scalarizer_kind, moo_surface.value_space
+    )
+
+    planner = BoTorchPlanner(
+        goal="minimize",
+        feas_strategy="naive-0",
+        init_design_strategy=init_design_strategy,
+        batch_size=batch_size,
+        use_descriptors=use_descriptors,
+        is_moo=True,
+        value_space=moo_surface.value_space,
+        scalarizer_kind=scalarizer_kind,
+        moo_params=moo_params,
+        goals=goals,
+    )
+
+    planner.set_param_space(moo_surface.param_space)
+
+    campaign = Campaign()
+    campaign.set_param_space(moo_surface.param_space)
+    campaign.set_value_space(moo_surface.value_space)
+
+    BUDGET = num_init_design + batch_size * 4
+
+    while len(campaign.observations.get_values()) < BUDGET:
+
+        samples = planner.recommend(campaign.observations)
+
+        for sample in samples:
+            sample_arr = sample.to_array()
+            measurement = moo_surface.run(sample_arr, return_paramvector=True)
+            campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
+
+    assert len(campaign.observations.get_params()) == BUDGET
+    assert len(campaign.observations.get_values()) == BUDGET
+    assert campaign.observations.get_values().shape[1] == len(
+        moo_surface.value_space
+    )
+
+
+def run_discrete(
+    init_design_strategy,
+    batch_size,
+    use_descriptors,
+    scalarizer_kind,
+    num_init_design=5,
+):
+
+    moo_surface = Surface(kind="MultFonseca")
+
+    param_space = ParameterSpace()
+    param_0 = ParameterDiscrete(
+        name="param_0", options=[0.0, 0.25, 0.5, 0.75, 1.0]
+    )
+    param_1 = ParameterDiscrete(
+        name="param_1", options=[0.0, 0.25, 0.5, 0.75, 1.0]
+    )
+    param_space.add(param_0)
+    param_space.add(param_1)
+
+    scalarizer, moo_params, goals = generate_scalarizer_object(
+        scalarizer_kind, moo_surface.value_space
+    )
+
+    planner = BoTorchPlanner(
+        goal="minimize",
+        feas_strategy="naive-0",
+        init_design_strategy=init_design_strategy,
+        batch_size=batch_size,
+        use_descriptors=use_descriptors,
+        is_moo=True,
+        value_space=moo_surface.value_space,
+        scalarizer_kind=scalarizer_kind,
+        moo_params=moo_params,
+        goals=goals,
+    )
+
+    planner.set_param_space(param_space)
+
+    campaign = Campaign()
+    campaign.set_param_space(param_space)
+    campaign.set_value_space(moo_surface.value_space)
+
+    BUDGET = num_init_design + batch_size * 4
+
+    while len(campaign.observations.get_values()) < BUDGET:
+
+        samples = planner.recommend(campaign.observations)
+
+        for sample in samples:
+            sample_arr = sample.to_array()
+            measurement = moo_surface.run(sample_arr, return_paramvector=True)
+            campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
+
+    assert len(campaign.observations.get_params()) == BUDGET
+    assert len(campaign.observations.get_values()) == BUDGET
+    assert campaign.observations.get_values().shape[1] == len(
+        moo_surface.value_space
+    )
+
+
+def run_mixed_cat_cont(
+    init_design_strategy,
+    batch_size,
+    use_descriptors,
+    scalarizer_kind,
+    num_init_design=5,
+):
+
+    moo_surface = Emulator(dataset="suzuki_i", model="BayesNeuralNet")
+
+    scalarizer, moo_params, goals = generate_scalarizer_object(
+        scalarizer_kind, moo_surface.value_space
+    )
+
+    planner = BoTorchPlanner(
+        goal="minimize",
+        feas_strategy="naive-0",
+        init_design_strategy=init_design_strategy,
+        batch_size=batch_size,
+        use_descriptors=use_descriptors,
+        is_moo=True,
+        value_space=moo_surface.value_space,
+        scalarizer_kind=scalarizer_kind,
+        moo_params=moo_params,
+        goals=goals,
+    )
+
+    planner.set_param_space(moo_surface.param_space)
+
+    campaign = Campaign()
+    campaign.set_param_space(moo_surface.param_space)
+    campaign.set_value_space(moo_surface.value_space)
+
+    BUDGET = num_init_design + batch_size * 4
+
+    while len(campaign.observations.get_values()) < BUDGET:
+
+        samples = planner.recommend(campaign.observations)
+
+        for sample in samples:
+            sample_arr = sample.to_array()
+            measurement = moo_surface.run(sample_arr, return_paramvector=True)
+
+            campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
+
+    assert len(campaign.observations.get_params()) == BUDGET
+    assert len(campaign.observations.get_values()) == BUDGET
+    assert campaign.observations.get_values().shape[1] == len(
+        moo_surface.value_space
+    )
+
+
 #
-# 	moo_surface = Surface(kind='MultFonseca')
+# def run_mixed_dis_cont(init_design_strategy, batch_size, use_descriptors, scalarizer_kind, num_init_design=5,):
+#
+#
+#
+# 	scalarizer, moo_params, goals = generate_scalarizer_object(scalarizer_kind, moo_surface.value_space)
 #
 # 	planner = BoTorchPlanner(
 # 		goal='minimize',
 # 		feas_strategy='naive-0',
-# 		init_design_strategy='lhs',
+# 		init_design_strategy=init_design_strategy,
+# 		batch_size=batch_size,
+# 		use_descriptors=use_descriptors,
 # 		is_moo=True,
 # 		value_space=moo_surface.value_space,
-# 		scalarizer_kind='Hypervolume',
-# 		moo_params={},
-# 		goals=['min', 'max']
+# 		scalarizer_kind=scalarizer_kind,
+# 		moo_params=moo_params,
+# 		goals=goals,
 # 	)
 #
-# 	scalarizer = Scalarizer(
-# 		kind='Hypervolume',
-# 		value_space=moo_surface.value_space,
-# 		goals=['min', 'max'],
-# 	)
 #
 # 	planner.set_param_space(moo_surface.param_space)
 #
@@ -116,8 +386,7 @@ SCALARIZER_KINDS = ["WeightedSum", "Parego", "Hypervolume", "Chimera"]
 # 	campaign.set_param_space(moo_surface.param_space)
 # 	campaign.set_value_space(moo_surface.value_space)
 #
-#
-# 	BUDGET = 10
+# 	BUDGET = num_init_design + batch_size*4
 #
 # 	while len(campaign.observations.get_values()) < BUDGET:
 #
@@ -126,165 +395,14 @@ SCALARIZER_KINDS = ["WeightedSum", "Parego", "Hypervolume", "Chimera"]
 # 		for sample in samples:
 # 			sample_arr = sample.to_array()
 # 			measurement = moo_surface.run(sample_arr, return_paramvector=True)
+#
 # 			campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
 #
 #
 # 	assert len(campaign.observations.get_params())==BUDGET
 # 	assert len(campaign.observations.get_values())==BUDGET
 # 	assert campaign.observations.get_values().shape[1] == len(moo_surface.value_space)
-#
-#
-# def test_continuous_parego():
-#
-# 	moo_surface = Surface(kind='MultFonseca')
-#
-#
-# 	planner = BoTorchPlanner(
-# 		goal='minimize',
-# 		feas_strategy='naive-0',
-# 		init_design_strategy='lhs',
-# 		is_moo=True,
-# 		value_space=moo_surface.value_space,
-# 		scalarizer_kind='Parego',
-# 		moo_params={},
-# 		goals=['min', 'max']
-# 	)
-#
-# 	scalarizer = Scalarizer(
-# 		kind='Parego',
-# 		value_space=moo_surface.value_space,
-# 		goals=['min', 'max'],
-# 	)
-#
-# 	planner.set_param_space(moo_surface.param_space)
-#
-# 	campaign = Campaign()
-# 	campaign.set_param_space(moo_surface.param_space)
-# 	campaign.set_value_space(moo_surface.value_space)
-#
-#
-# 	BUDGET = 10
-#
-# 	while len(campaign.observations.get_values()) < BUDGET:
-#
-# 		samples = planner.recommend(campaign.observations)
-#
-# 		for sample in samples:
-# 			sample_arr = sample.to_array()
-# 			measurement = moo_surface.run(sample_arr, return_paramvector=True)
-# 			campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
-#
-#
-# 	assert len(campaign.observations.get_params())==BUDGET
-# 	assert len(campaign.observations.get_values())==BUDGET
-# 	assert campaign.observations.get_values().shape[1] == len(moo_surface.value_space)
-#
-#
-# def test_continuous_weighted_sum():
-#
-# 	moo_surface = Surface(kind='MultFonseca')
-#
-#
-# 	planner = BoTorchPlanner(
-# 		goal='minimize',
-# 		feas_strategy='naive-0',
-# 		init_design_strategy='lhs',
-# 		is_moo=True,
-# 		value_space=moo_surface.value_space,
-# 		scalarizer_kind='WeightedSum',
-# 		moo_params={'weights': [2., 1.]},
-# 		goals=['min', 'max']
-# 	)
-#
-# 	scalarizer = Scalarizer(
-# 		scalarizer_kind='WeightedSum',
-# 		value_space=moo_surface.value_space,
-# 		moo_params={'weights': [2., 1.]},
-# 		goals=['min', 'max']
-# 	)
-#
-# 	planner.set_param_space(moo_surface.param_space)
-#
-# 	campaign = Campaign()
-# 	campaign.set_param_space(moo_surface.param_space)
-# 	campaign.set_value_space(moo_surface.value_space)
-#
-#
-# 	BUDGET = 10
-#
-# 	while len(campaign.observations.get_values()) < BUDGET:
-#
-# 		samples = planner.recommend(campaign.observations)
-#
-# 		for sample in samples:
-# 			sample_arr = sample.to_array()
-# 			measurement = moo_surface.run(sample_arr, return_paramvector=True)
-# 			campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
-#
-#
-# 	assert len(campaign.observations.get_params())==BUDGET
-# 	assert len(campaign.observations.get_values())==BUDGET
-# 	assert campaign.observations.get_values().shape[1] == len(moo_surface.value_space)
-#
-#
-# def test_continuous_chimera():
-#
-# 	moo_surface = Surface(kind='MultFonseca')
-#
-#
-# 	planner = BoTorchPlanner(
-# 		goal='minimize',
-# 		feas_strategy='naive-0',
-# 		init_design_strategy='lhs',
-# 		is_moo=True,
-# 		value_space=moo_surface.value_space,
-# 		scalarizer_kind='Chimera',
-# 		moo_params={'absolutes': [True, True], 'tolerances': [0.5, 0.5]},
-# 		goals=['min', 'max']
-# 	)
-#
-# 	scalarizer = Scalarizer(
-# 		kind='Chimera',
-# 		value_space=moo_surface.value_space,
-# 		goals=['min', 'max'],
-# 		absolutes=[True, True],
-# 		tolerances=[0.5, 0.5],
-# 	)
-#
-# 	planner.set_param_space(moo_surface.param_space)
-#
-# 	campaign = Campaign()
-# 	campaign.set_param_space(moo_surface.param_space)
-# 	campaign.set_value_space(moo_surface.value_space)
-#
-#
-# 	BUDGET = 10
-#
-# 	while len(campaign.observations.get_values()) < BUDGET:
-#
-# 		samples = planner.recommend(campaign.observations)
-#
-# 		for sample in samples:
-# 			sample_arr = sample.to_array()
-# 			measurement = moo_surface.run(sample_arr, return_paramvector=True)
-# 			campaign.add_and_scalarize(sample_arr, measurement, scalarizer)
-#
-#
-# 	assert len(campaign.observations.get_params())==BUDGET
-# 	assert len(campaign.observations.get_values())==BUDGET
-# 	assert campaign.observations.get_values().shape[1] == len(moo_surface.value_space)
-#
-#
-# def run_continuous(
-# 	init_design_strategy, batch_size, use_descriptors, scalarizer, num_init_design=5,
-# ):
 
 
-# if __name__ == "__main__":
-#
-#     # test_continuous_weighted_sum()
-#     # test_continuous_hypervolume()
-#     # test_continuous_chimera()
-#     # test_continuous_parego()
-#
-#     pass
+if __name__ == "__main__":
+    pass
