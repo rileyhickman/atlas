235633ecf3f576fae648ed611ea0012fb2b7f327
rileyhickman <riley.hickman13@gmail.com>
2023-04-10 14:29:16 -0400

    formulating medusa ga

--- src/atlas/optimizers/acquisition_optimizers/genetic_optimizer.py
+++ src/atlas/optimizers/acquisition_optimizers/genetic_optimizer.py
@@ -4,35 +4,23 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
+from botorch.acquisition import AcquisitionFunction
 from deap import base, creator, tools
+from olympus import ParameterVector
+from olympus.campaigns import ParameterSpace
 from rich.progress import track
 
 from atlas import Logger
-from atlas.optimizers.utils import param_vector_to_dict
-
-from botorch.acquisition import AcquisitionFunction
-
-from olympus import ParameterVector
-from olympus.campaigns import ParameterSpace
-
-
-from atlas.optimizers.acqfs import (
-    create_available_options,
-)
+from atlas.optimizers.acqfs import create_available_options
+from atlas.optimizers.acquisition_optimizers.base_optimizer import \
+    AcquisitionOptimizer
 from atlas.optimizers.params import Parameters
-from atlas.optimizers.utils import (
-    cat_param_to_feat,
-    forward_normalize,
-    forward_standardize,
-    get_cat_dims,
-    get_fixed_features_list,
-    infer_problem_type,
-    propose_randomly,
-    reverse_normalize,
-    reverse_standardize,
-)
-from atlas.optimizers.acquisition_optimizers.base_optimizer import AcquisitionOptimizer
-
+from atlas.optimizers.utils import (cat_param_to_feat, forward_normalize,
+                                    forward_standardize, get_cat_dims,
+                                    get_fixed_features_list,
+                                    infer_problem_type, param_vector_to_dict,
+                                    propose_randomly, reverse_normalize,
+                                    reverse_standardize)
 
 
 class GeneticOptimizer(AcquisitionOptimizer):
@@ -116,7 +104,7 @@ class GeneticOptimizer(AcquisitionOptimizer):
                 param_ranges.append(len(param.options))
                 if self.has_descriptors:
                     counter+=len(param.descriptors[0])
-                else:
+                else:``
                     counter+=len(param.options)
         return np.array(param_ranges)
 
