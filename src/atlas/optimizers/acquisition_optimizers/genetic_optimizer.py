fix genetic acqf opt scaling bug

src/atlas/optimizers/acquisition_optimizers/genetic_optimizer.py
@@ -315,16 +315,20 @@ class GeneticOptimizer(AcquisitionOptimizer):
         best_idxs = np.argsort(acqf_vals)[:self.batch_size]
         best_batch_pop = np.array(population)[best_idxs]
 
-        # print(np.array(population)[:10])
-        # print(best_batch_pop[:10])
-        # print(acqf_vals[:10])
-        # print(np.array(acqf_vals)[best_idxs])
 
         # TODO: this is pretty hacky...
         # Does this re-normalize things??
-        best_batch_pop_deindex = self.deindexify(best_batch_pop)
+        best_batch_pop_deindex = self.deindexify(best_batch_pop) # scaled
 
-        best_batch_pop_deindex = reverse_normalize(best_batch_pop_deindex,self.params_obj._mins_x,self.params_obj._maxs_x)
+
+        best_batch_pop_deindex = reverse_normalize(
+            best_batch_pop_deindex, 
+            self.params_obj._mins_x,
+            self.params_obj._maxs_x,
+            #np.array(self.params_obj.bounds[0]), # mins
+            #np.array(self.params_obj.bounds[1]), # maxs
+        )
+
 
         best_batch = []
         for best_index, best_deindex in zip(best_batch_pop,best_batch_pop_deindex):
@@ -588,22 +592,24 @@ class GeneticOptimizer(AcquisitionOptimizer):
 
         assert len(individual) == len(self.param_space)
 
+        bounds_ix = 0 # tracks the index of the bounds
+
         for i, param in enumerate(self.param_space):
             param_type = param["type"]
 
-            # determine whether we are performing a mutation
-            if np.random.random() < indpb:
-
-                if param_type == "continuous":
+            if param_type == "continuous":
+                if np.random.random() < indpb:
                     # Gaussian perturbation with scale being 0.1 of domain range
-                    bound_low = self.bounds[0,i]
-                    bound_high = self.bounds[1,i]
+                    bound_low = self.bounds[0,bounds_ix]
+                    bound_high = self.bounds[1,bounds_ix]
                     scale = (bound_high - bound_low) * continuous_scale
                     individual[i] += np.random.normal(loc=0.0, scale=scale)
                     individual[i] = _project_bounds(
                         individual[i], bound_low, bound_high
                     )
-                elif param_type == "discrete":
+                bounds_ix+=1
+            elif param_type == "discrete":
+                if np.random.random() < indpb:
                     # add/substract an integer by rounding Gaussian perturbation
                     #scale is 0.1 of domain range
                     bound_low = 0
@@ -619,8 +625,11 @@ class GeneticOptimizer(AcquisitionOptimizer):
                     individual[i] = _project_bounds(
                         individual[i], bound_low, bound_high
                     )
+                bounds_ix+=1
 
-                elif param_type == "categorical":
+            elif param_type == "categorical":
+                
+                if np.random.random() < indpb:
                     # resample a random category
                     num_options = float(
                         self.param_ranges[i]
@@ -628,10 +637,9 @@ class GeneticOptimizer(AcquisitionOptimizer):
                     individual[i] = np.random.choice(
                         list(np.arange(num_options))
                     )
-                else:
-                    raise ValueError()
+                bounds_ix+=int(self.param_ranges[i])
             else:
-                continue
+                raise ValueError()
 
         return (individual,)
 